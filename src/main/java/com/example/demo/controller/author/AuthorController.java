d5387f9fd8388c8706d499f17be343459b6d7dbf
Siddique <sayedsiddique02@gmail.com>
2025-07-08 17:46:57 +0530

    refactor: api response for authors

--- src/main/java/com/example/demo/controller/author/AuthorController.java
+++ src/main/java/com/example/demo/controller/author/AuthorController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller.author;
 
 import com.example.demo.dto.author.AuthorDTO;
+import com.example.demo.dto.response.ApiResponse;
 import com.example.demo.service.author.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +21,9 @@ public class AuthorController {
     }
 
     @GetMapping
-    public List<AuthorDTO> getAuthors() {
-        return authorService.getAuthors();
+    public ResponseEntity<ApiResponse<List<AuthorDTO>>> getAuthors() {
+        List<AuthorDTO> authors = authorService.getAuthors();
+        ApiResponse<List<AuthorDTO>> response = new ApiResponse<>("Authors retrieved successfully", "success", HttpStatus.OK.value(), authors);
+        return new ResponseEntity<ApiResponse<List<AuthorDTO>>>(response, HttpStatus.OK);
     }
 }
